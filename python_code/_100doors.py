26773b5c6b8eca9b19e3e4cecc7dd0c2a689c0f7
Tiago Correia Gonçalves <tcorreia@usersad.everis.int>
2019-08-05 14:42:32 +0100

    Adicionado jogo das portas

--- python_code/_100doors.py
+++ python_code/_100doors.py
@@ -1,18 +1,20 @@
-'''
-There are 100 doors in a row that are all initially closed. 
-You make 100 passes by the doors. 
-The first time through, visit every door and toggle the door 
-(if the door is closed, open it; if it is open, close it). 
-The second time, only visit every 2nd door 
-(door #2, #4, #6, ...), and toggle it. 
-The third time, visit every 3rd door (door #3, #6, #9, ...), etc, 
-until you only visit the 100th door.  
-Answer the question: what state are the doors in after 
-the last pass?  
-Which are open, which are closed?  
-Alternate: As noted in this page's discussion page, the only 
-doors that remain open are those whose numbers are perfect squares. 
-Opening only those doors is an optimization that may also be  
-expressed; however, as should be obvious, this defeats the intent 
-of comparing implementations across programming languages.  
-'''
+#python3
+
+def doors():
+	portas = []
+	for i in range(100):
+		portas.append("fechada")
+
+	for i in range(1,100):
+		for j in range(i-1,100,i):
+			portas[j]=toogle(portas[j])
+	print(portas)
+		
+def toogle(porta):
+	if(porta== "fechada"):
+		porta="aberta"
+	else:
+		porta= "fechada"
+	return porta
+
+doors()
